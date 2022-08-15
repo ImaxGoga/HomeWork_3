042233becbc338e5623fb32e4f69221283545375
ImaxGoga <45959194+ImaxGoga@users.noreply.github.com>
2022-08-15 13:44:31 +0300

    First request done!

--- 3_1/Program.cs
+++ 3_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿ Console.Write("Введите пятизначное число: ");
+string a1 = Console.ReadLine();
+int size = a1.Length;
+bool result = true;
+for(int i = 0; i < size /2 ; i++)
+{
+    if (a1[i] != a1[size - 1 -i] )
+    {
+        result = false;
+        break;
+    }
+}
+if (result) {
+    Console.WriteLine("Да, это палиндром!");
+    }
+else {
+    Console.WriteLine("Нет, это не палиндром!");
+}
